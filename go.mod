b5a78e4e20f4f66d361c28b3c7ab16b5afe22983
John Beshir <john@beshir.org>
2018-11-09 11:48:46 +0000

    Better error reporting, minimal styling.

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/jbeshir/moonbird-predictor-frontend
 require (
 	cloud.google.com/go v0.32.0 // indirect
 	github.com/pkg/errors v0.8.0
-	golang.org/x/net v0.0.0-20181102091132-c10e9556a7bc // indirect
-	golang.org/x/oauth2 v0.0.0-20181102170140-232e45548389
-	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f // indirect
-	google.golang.org/api v0.0.0-20181102150758-04bb50b6b83d
+	golang.org/x/net v0.0.0-20181108082009-03003ca0c849 // indirect
+	golang.org/x/oauth2 v0.0.0-20181106182150-f42d05182288
+	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f // indirect
+	google.golang.org/api v0.0.0-20181108001712-cfbc873f6b93
 	google.golang.org/appengine v1.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -17,15 +17,19 @@ import (
 )
 
 var indexTemplate = template.Must(template.New("index").Parse(
-`<html>
-<body>
+	`<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Slab" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="/static/moonbird.css" />
+</head>
+<body class="predict-page">
 <h1>Moonbird Predictor</h1>
-<form id="prediction-series-input" action="/">
+<form id="prediction-form" action="/">
 	<div>Input a comma-separated series of human-assigned probabilties to get Moonbird Predictor's best guess at the likelihood of the event happening. Slightly outperforms naive averaging in validation against PredictionBook data!</div>
-	<input type="text" placeholder="Probabilities go here..." name="assignments" value="{{.AssignmentsStr}}"></input>
+	<input type="text" placeholder="Probabilities go here..." name="assignments" value="{{.AssignmentsStr}}" class="prediction-text-input"></input>
+{{if .Err}}<div class="prediction-fault-msg">Fault predicting using given sequence!<div id="prediction-fault">{{.Err}}</div></div>{{end}}
+{{if .Prediction}}<div class="prediction-result-msg"><div class="prediction-result-title">Predicted Likelihood</div><div class="prediction-result">{{.Prediction}}</div></div>{{end}}
 </form>
-{{if .Err}}<div id="prediction-error">Unable to predict using given sequence! Error was: {{.Err}}</div>{{end}}
-{{if .Prediction}}<div id="prediction-result-msg">Predicted likelihood: <span id="prediction-result">{{.Prediction}}</span>!</div>{{end}}
 </body>
 </html>`))
 
@@ -71,12 +75,12 @@ func handle(w http.ResponseWriter, r *http.Request) {
 
 	indexTemplate.Execute(w, &struct {
 		AssignmentsStr string
-		Prediction *float64
-		Err        error
-	} {
+		Prediction     *float64
+		Err            error
+	}{
 		AssignmentsStr: r.FormValue("assignments"),
-		Prediction: prediction,
-		Err: err,
+		Prediction:     prediction,
+		Err:            err,
 	})
 }
 
@@ -96,40 +100,49 @@ type resultPrediction struct {
 }
 
 func makePrediction(ctx context.Context, predictions [][1]float64) (float64, error) {
-	req, err := newMLRequest(predictions)
-	if err != nil {
-		return 0, err
-	}
-
 	client, err := google.DefaultClient(ctx, ml.CloudPlatformScope)
 	if err != nil {
-		return 0, err
+		return 0, errors.Wrap(err, "makePrediction couldn't create client")
+	}
+
+	req, err := newMLRequest(predictions)
+	if err != nil {
+		return 0, errors.Wrap(err, "makePrediction couldn't create request")
 	}
 
 	s, err := ml.New(client)
 	if err != nil {
-		return 0, err
+		return 0, errors.Wrap(err, "makePrediction couldn't create service")
 	}
 
 	mlPredictCall := s.Projects.Predict("projects/moonbird-beshir/models/Predictor", req)
 	r, err := mlPredictCall.Context(ctx).Do()
 	if err != nil {
-		return 0, err
+		return 0, errors.Wrap(err, "makePrediction couldn't run request")
+	}
+	if r.HTTPStatusCode != http.StatusOK {
+		return 0, errors.Errorf("makePrediction couldn't run request, status code: %d", r.HTTPStatusCode)
 	}
 
 	var result result
 	err = json.NewDecoder(strings.NewReader(r.Data)).Decode(&result)
 	if err != nil {
-		return 0, err
+		return 0, errors.Wrap(err, "makePrediction couldn't decode response")
 	}
 	if len(result.Predictions) != 1 || len(result.Predictions[0].Income) != 1 {
-		return 0, errors.New("malformed predict response: Did not get one and only one probability")
+		return 0, errors.New("makePrediction got malformed predict response: Did not get one and only one probability")
 	}
 
 	return result.Predictions[0].Income[0], nil
 }
 
 func newMLRequest(predictions [][1]float64) (*ml.GoogleCloudMlV1__PredictRequest, error) {
+
+	for _, p := range predictions {
+		if p[0] < 0 || p[0] > 1 {
+			return nil, errors.Errorf("Probability assignment out of range: %g", p[0])
+		}
+	}
 
 	jsonreq := request{
 		Instances: []requestInput{
